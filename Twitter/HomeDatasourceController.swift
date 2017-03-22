0041969373a9fba3e174e0924a496031f3553a41
morphley <developer.omari@gmail.com>
2017-03-22 15:53:57 +0100

    Video 7: Build custom Navigation Bar

--- Twitter/HomeDatasourceController.swift
+++ Twitter/HomeDatasourceController.swift
@@ -13,9 +13,39 @@ class HomeDatasourceController: DatasourceController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupNavigationBarItems()
+        
         let homeDateSource = HomeDatasource()
         self.datasource = homeDateSource // 3 items in my collectionView
     }
+    
+    
+    private func setupNavigationBarItems(){
+        
+        let titleImageView = UIImageView(image: #imageLiteral(resourceName: "title_icon"))
+        //bird is too big must be shrinked down
+        titleImageView.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        titleImageView.contentMode = .scaleAspectFit
+        navigationItem.titleView = titleImageView
+        
+        let followButton = UIButton(type: .system)
+        followButton.setImage(#imageLiteral(resourceName: "follow").withRenderingMode(.alwaysOriginal), for: .normal)
+        followButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
+        
+        
+        let searchButton = UIButton(type: .system)
+        searchButton.setImage(#imageLiteral(resourceName: "search").withRenderingMode(.alwaysOriginal), for: .normal)
+        searchButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        let composeButton = UIButton(type: .system)
+        composeButton.setImage(#imageLiteral(resourceName: "compose").withRenderingMode(.alwaysOriginal), for: .normal)
+        composeButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: composeButton),UIBarButtonItem(customView: searchButton)] // array goes left to right
+    }
+    
     
     //gap between two cells
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -29,9 +59,9 @@ class HomeDatasourceController: DatasourceController {
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         if let user = self.datasource?.item(indexPath) as? User {
-         //   user.bioText
-        // estimation for the height of our cells based on user.bioText
-           
+            //   user.bioText
+            // estimation for the height of our cells based on user.bioText
+            
             let approximatedWidthOfBioTextView = view.frame.width - 12-50-12-2
             let size = CGSize(width: approximatedWidthOfBioTextView, height: 1000)
             
